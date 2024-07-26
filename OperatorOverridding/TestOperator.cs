bd95e04e5c7333d6ff1d03ad1d9ad838baf7aa50
Abhishek <Abhishekgwari14sep@gmail.com>
2024-07-26 20:40:42 +0530

    Add project files.

---
+++ OperatorOverridding/TestOperator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverridding
+{
+    internal class TestOperator
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hello";
+            string s2 = "World";
+
+            Console.WriteLine(s1 + s2);
+        //    Console.WriteLine(s1 - s2);
+
+
+            Console.ReadLine();
+        }
+       
+    }
+}
